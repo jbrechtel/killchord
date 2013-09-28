e2f580d13455d58f7a006477a71026fc1d089ce7
James Brechtel <jbrechtel@gmail.com>
2013-09-27 20:37:13 -0400

    Adding dependencies

--- project.clj
+++ project.clj
@@ -3,5 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [compojure "1.1.5"]
+                 [enlive "1.1.4"]
+                 [org.bouncycastle/bcprov-jdk16 "1.46"]
+                 [com.datomic/datomic-free "0.8.4159"]]
   :main kill-chord.core)
