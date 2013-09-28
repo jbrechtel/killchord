e850aa56992349aedb0a5a373f0627094b0429fc
James Brechtel <jbrechtel@gmail.com>
2013-09-27 20:30:36 -0400

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to kill_chord
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/kill_chord/core_test.clj
@@ -0,0 +1,7 @@
+(ns kill-chord.core-test
+  (:require [clojure.test :refer :all]
+            [kill-chord.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
