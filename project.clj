f3f6686e47d19d8d0c48da9da0b36a9f294129a2
Stephen <stephen@7thg.com>
2013-09-28 22:16:03 -0400

    Removing dependency on BouncyCastle.

--- project.clj
+++ project.clj
@@ -6,7 +6,6 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]
                  [enlive "1.1.4"]
-                 [org.bouncycastle/bcprov-jdk16 "1.46"]
                  [com.datomic/datomic-free "0.8.4159"]
                  [ring/ring-jetty-adapter "1.2.0"]
                  [clj-time "0.6.0"]]
