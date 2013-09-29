9b73286dde4c113c490b0614509c7616ec98e2ae
Stephen <stephen@7thg.com>
2013-09-28 22:16:24 -0400

    Adding heartbeat POST route.

--- src/kill_chord/handler.clj
+++ src/kill_chord/handler.clj
@@ -4,11 +4,15 @@
             [compojure.route :as route]
             [kill-chord.templates :as templates]
             [clj-time.core :as time]
-            [clj-time.format :as time-format]))
+            [clj-time.format :as time-format]
+            [clojure.java.io :as io]))
 
 (defroutes app-routes
   (GET "/" [] (clojure.string/join (templates/index-template "Kill Chord")))
   (GET "/heartbeat" [] (time-format/unparse (time-format/formatters :rfc822) (time/now)))
+  (POST "/heartbeat" [:as r]
+    (with-open [rdr (io/reader (:body r))]
+      (clojure.string/join "\n" (line-seq rdr))))
   (route/resources "/")
   (route/not-found "Not Found"))
 
